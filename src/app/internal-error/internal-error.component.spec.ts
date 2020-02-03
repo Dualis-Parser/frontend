f605186075daa465b9706d14a342f0cfff19c51b
Leonhard Gahr <leonhard.gahr@siemens.com>
2020-02-03 09:58:12 +0100

    fix karma tests

--- src/app/internal-error/internal-error.component.spec.ts
+++ src/app/internal-error/internal-error.component.spec.ts
@@ -1,6 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { InternalErrorComponent } from './internal-error.component';
+import {InternalErrorComponent} from './internal-error.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('InternalErrorComponent', () => {
   let component: InternalErrorComponent;
@@ -8,9 +10,10 @@ describe('InternalErrorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InternalErrorComponent ]
+      declarations: [InternalErrorComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
